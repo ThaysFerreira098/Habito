010883794b3265fecbabab10e7b93093a9bacf4c
ElisaKoepp <elisakoepp123@gmail.com>
2025-11-05 16:14:21 -0300

    feat: adicionado Service

---
+++ habito/src/main/java/com/info/habito/service/HabitoService.java
@@ -0,0 +1,31 @@
+package com.info.habito.service;
+import com.info.habito.model.Habito;
+import com.info.habito.repository.HabitoRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HabitoService {
+    @Autowired
+ HabitoRepository habitoRepository;
+
+ public Habito localizar(int id){
+ Optional<Habito> habitoBusca = habitoRepository.findById(id);
+ if (habitoBusca.isPresent()) {
+ return habitoBusca.get();
+ } else {
+ return null;
+ }
+ }
+
+ public Habito salvar(Habito habito) {
+ return habitoRepository.save(habito);
+ }
+
+ public void concluir(int id) {
+ 
+    
+ }
+}
